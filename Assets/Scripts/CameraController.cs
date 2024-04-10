6930a28cb094a93788d61e6fc20345c782a8a52d
KinnaGt <devilboogey@gmail.com>
2024-04-10 12:25:27 -0300

    Add camera movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,13 +8,22 @@ public class CameraController : MonoBehaviour
     Transform followObject;
 
     [SerializeField]
-    Vector3 offset = new Vector3(0, 2, -5); // Desplazamiento detrás del objeto
+    Vector3 offset = new Vector3(0, 2, -5);
+
+    float currentAngle = 0f;
 
     void Update()
     {
         if (followObject != null)
         {
-            transform.position = followObject.position + offset;
+            if (Input.GetMouseButton(0))
+            {
+                currentAngle += Input.GetAxis("Mouse X");
+            }
+
+            Quaternion rotation = Quaternion.Euler(0, currentAngle, 0);
+            transform.position = followObject.position + rotation * offset;
+
             transform.LookAt(followObject);
         }
     }
